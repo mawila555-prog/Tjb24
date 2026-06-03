fb4eacc4bfa8bef741e76eb491edd93c0da2092d
mawila555-prog <mawila555@gmail.com>
2026-06-03 22:58:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,76 @@
-document
-.getElementById("startBtn")
-.addEventListener("click", () => {
+const startBtn = document.getElementById("startBtn");
+const music = document.getElementById("bgMusic");
+const surpriseBtn = document.getElementById("surpriseBtn");
+const secretMessage = document.getElementById("secretMessage");
 
-    window.scrollTo({
-        top: window.innerHeight,
+startBtn.addEventListener("click", () => {
+
+```
+music.play();
+
+document.querySelector(".journey")
+    .scrollIntoView({
         behavior: "smooth"
     });
+```
 
 });
+
+surpriseBtn.addEventListener("click", () => {
+
+```
+secretMessage.style.display = "block";
+
+createHeartBurst();
+```
+
+});
+
+function createHeartBurst() {
+
+```
+for(let i = 0; i < 25; i++){
+
+    const heart = document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML = "❤️";
+
+    heart.style.left =
+        Math.random() * window.innerWidth + "px";
+
+    heart.style.bottom = "0px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 4000);
+}
+```
+
+}
+
+setInterval(() => {
+
+```
+const heart = document.createElement("div");
+
+heart.classList.add("heart");
+
+heart.innerHTML = "❤️";
+
+heart.style.left =
+    Math.random() * window.innerWidth + "px";
+
+heart.style.bottom = "-20px";
+
+document.body.appendChild(heart);
+
+setTimeout(() => {
+    heart.remove();
+}, 4000);
+```
+
+}, 1500);
